ac43906341b1f1dd5aed417866ac4225dddfccd2
Mike Casey <hi@mcasey.xyz>
2025-11-25 07:47:43 -0500

    Launching site

--- site/src/utils/releaseDate.js
+++ site/src/utils/releaseDate.js
@@ -4,7 +4,7 @@
  * Each subsequent day releases at midnight UTC
  */
 
-const START_DATE = new Date('2025-11-25T00:00:00Z'); // Tuesday, November 26, 2025
+const START_DATE = new Date('2025-11-24T00:00:00Z');
 
 export function isReleased(dayNumber) {
   const releaseDate = getReleaseDate(dayNumber);
